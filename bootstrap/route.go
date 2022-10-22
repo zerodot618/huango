9c9084f38e390d54cdcd5f22585af1487b5eba68
huanggz <918031078@qq.com>
2022-10-22 16:49:58 +0800

    http use custom logger

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -4,6 +4,7 @@ package bootstrap
 import (
 	"net/http"
 	"strings"
+	"zerodot618/huango/app/http/middlewares"
 	"zerodot618/huango/routes"
 
 	"github.com/gin-gonic/gin"
@@ -24,7 +25,8 @@ func SetupRoute(router *gin.Engine) {
 
 func registerGlobalMiddleWare(router *gin.Engine) {
 	router.Use(
-		gin.Logger(),
+		// gin.Logger(),
+		middlewares.Logger(),
 		gin.Recovery(),
 	)
 }
