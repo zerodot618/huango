82b15341bf19b8e220219f401c72b9638b0a9146
huanggz <918031078@qq.com>
2022-10-23 14:04:40 +0800

    user list api

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"zerodot618/huango/app/models/user"
 	"zerodot618/huango/pkg/auth"
 	"zerodot618/huango/pkg/response"
 
@@ -15,4 +16,10 @@ type UsersController struct {
 func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 	userModel := auth.CurrentUser(c)
 	response.Data(c, userModel)
+}
+
+// Index 所有用户
+func (ctrl *UsersController) Index(c *gin.Context) {
+	data := user.All()
+	response.Data(c, data)
 }
