46a6928141b2597c4807b39c9968e1aa9be344eb
huanggz <918031078@qq.com>
2022-10-23 11:43:26 +0800

    jwt authorization

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,6 +2,7 @@
 package app
 
 import (
+	"time"
 	"zerodot618/huango/pkg/config"
 )
 
@@ -15,4 +16,10 @@ func IsProduction() bool {
 
 func IsTesting() bool {
     return config.Get("app.env") == "testing"
+}
+
+// TimenowInTimezone 获取当前时间，支持时区
+func TimenowInTimezone() time.Time {
+    chinaTimezone, _ := time.LoadLocation(config.GetString("app.timezone"))
+    return time.Now().In(chinaTimezone)
 }
