4c68e996897bdfa813d788bb77632c14e773d45e
huanggz <918031078@qq.com>
2022-10-23 13:32:24 +0800

    migrate rollback command

--- app/cmd/migrate.go
+++ app/cmd/migrate.go
@@ -8,30 +8,43 @@ import (
 )
 
 var CmdMigrate = &cobra.Command{
-    Use:   "migrate",
-    Short: "Run database migration",
-    // 所有 migrate 下的子命令都会执行以下代码
+	Use:   "migrate",
+	Short: "Run database migration",
+	// 所有 migrate 下的子命令都会执行以下代码
 }
 
 var CmdMigrateUp = &cobra.Command{
-    Use:   "up",
-    Short: "Run unmigrated migrations",
-    Run:   runUp,
+	Use:   "up",
+	Short: "Run unmigrated migrations",
+	Run:   runUp,
 }
 
 func init() {
-    CmdMigrate.AddCommand(
-        CmdMigrateUp,
-    )
+	CmdMigrate.AddCommand(
+		CmdMigrateUp,
+		CmdMigrateRollback,
+	)
 }
 
 func migrator() *migrate.Migrator {
-    // 注册 database/migrations 下的所有迁移文件
-    migrations.Initialize()
-    // 初始化 migrator
-    return migrate.NewMigrator()
+	// 注册 database/migrations 下的所有迁移文件
+	migrations.Initialize()
+	// 初始化 migrator
+	return migrate.NewMigrator()
 }
 
 func runUp(cmd *cobra.Command, args []string) {
-    migrator().Up()
+	migrator().Up()
+}
+
+var CmdMigrateRollback = &cobra.Command{
+	Use: "down",
+	// 设置别名 migrate down == migrate rollback
+	Aliases: []string{"rollback"},
+	Short:   "Reverse the up command",
+	Run:     runDown,
+}
+
+func runDown(cmd *cobra.Command, args []string) {
+	migrator().Rollback()
 }
