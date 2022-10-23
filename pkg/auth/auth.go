f5be5e1048b166b39533ca62f4b950360cb8fcfe
huanggz <918031078@qq.com>
2022-10-23 12:04:03 +0800

    auth middleware

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -4,28 +4,47 @@ package auth
 import (
 	"errors"
 	"zerodot618/huango/app/models/user"
+	"zerodot618/huango/pkg/logger"
+
+	"github.com/gin-gonic/gin"
 )
 
 // Attempt 尝试登录
 func Attempt(email string, password string) (user.User, error) {
-    userModel := user.GetByMulti(email)
-    if userModel.ID == 0 {
-        return user.User{}, errors.New("账号不存在")
-    }
+	userModel := user.GetByMulti(email)
+	if userModel.ID == 0 {
+		return user.User{}, errors.New("账号不存在")
+	}
 
-    if !userModel.ComparePassword(password) {
-        return user.User{}, errors.New("密码错误")
-    }
+	if !userModel.ComparePassword(password) {
+		return user.User{}, errors.New("密码错误")
+	}
 
-    return userModel, nil
+	return userModel, nil
 }
 
 // LoginByPhone 登录指定用户
 func LoginByPhone(phone string) (user.User, error) {
-    userModel := user.GetByPhone(phone)
-    if userModel.ID == 0 {
-        return user.User{}, errors.New("手机号未注册")
-    }
+	userModel := user.GetByPhone(phone)
+	if userModel.ID == 0 {
+		return user.User{}, errors.New("手机号未注册")
+	}
 
-    return userModel, nil
+	return userModel, nil
+}
+
+// CurrentUser 从 gin.context 中获取当前登录用户
+func CurrentUser(c *gin.Context) user.User {
+	userModel, ok := c.MustGet("current_user").(user.User)
+	if !ok {
+		logger.LogIf(errors.New("无法获取用户"))
+		return user.User{}
+	}
+	// db is now a *DB value
+	return userModel
+}
+
+// CurrentUID 从 gin.context 中获取当前登录用户 ID
+func CurrentUID(c *gin.Context) string {
+	return c.GetString("current_user_id")
 }
