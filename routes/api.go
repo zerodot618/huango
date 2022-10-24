1e5c33400287dc38441fd0a57298905b52323d38
huanggz <918031078@qq.com>
2022-10-24 08:21:21 +0800

    api domain and prefix

--- routes/api.go
+++ routes/api.go
@@ -6,6 +6,7 @@ import (
 	controllers "zerodot618/huango/app/http/controllers/api/v1"
 	"zerodot618/huango/app/http/controllers/api/v1/auth"
 	"zerodot618/huango/app/http/middlewares"
+	"zerodot618/huango/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,7 +15,12 @@ import (
 func RegisterAPIRoutes(r *gin.Engine) {
 
 	// v1 版本路由组，我们所有的 v1 版本的路由都将存放到这里
-	v1 := r.Group("/v1")
+	var v1 *gin.RouterGroup
+	if len(config.Get("app.api_domain")) == 0 {
+		v1 = r.Group("/api/v1")
+	} else {
+		v1 = r.Group("/v1")
+	}
 
 	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
 	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
